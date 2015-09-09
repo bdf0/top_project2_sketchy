a4687df241efbd293f5bc528c96e96f8edb4713a
Ben Freeman <djregn@gmail.com>
2015-09-09 17:32:09 -0500

    Added color picker

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 var sketchsize = 600;
-var rows = 48;
-var cols = 48;
+var rows = 16;
+var cols = 16;
+var pixelsize = sketchsize / rows;
+var margin = parseInt(-rows / 7);
 
-$(window).load(function() {
+var bcolor = "gray";
+var icolor = "black";
+
+
+
+$(document).ready(function() {
 
     renderSketchArea();
 
-    $(".pixel").hover(function() {
-        $(this).addClass("marked");
+    
+    $(".bcolor").click(function () {
+        $(".unmarked").css("background-color", $(this).css("background-color"))
     });
+    
+    $(".icolor").click(function() {
+        icolor = $(this).css("background-color");
+        $(".marked").css("background-color", icolor)
+
+    });
+        
+    $(".pixel").hover(function() {
+        $(this).removeClass("unmarked");
+        $(this).addClass("marked");
+        $(this).css("background-color", icolor);
+    });
+
+    
 });
 
+
 var renderSketchArea = function() {
+    $("#sketch").empty();
+    
     for (var i = 0; i < rows; i++) {
         $("#sketch").append("<div class=\"row\" id=\"row" + i + "\"></div>");
+        
         for (var j = 0; j < cols; j++) {
-            $("#row" + i).append("<div class=\"pixel\"></div>");
+            $("#row" + i).append("<div class=\"pixel unmarked\"></div>");
         }
     }
     
-    $(".pixel").width(sketchsize / rows);
-    $(".pixel").height(sketchsize / cols);
-    
+    $(".pixel").width(pixelsize);
+    $(".pixel").height(pixelsize);
+    $(".pixel").addClass(bcolor);
+    $(".row").css("margin-top", margin + "px");
 }
